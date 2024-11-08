41aacfbcb53dce978470eeaefca85d9c3df8b2d2
developer-shourav <mrinmoyraj823@gmail.com>
2024-11-08 09:39:12 +0600

    Problem 5 solved

---
+++ problem-5.ts
@@ -0,0 +1,7 @@
+/* ----------GetProperty Generic Function For Getting Property Value --------- */
+const getProperty = <T, K extends keyof T>(obj: T, key: K): T[K] => {
+  return obj[key];
+};
+
+const person = { name: "Alice", age: 30 };
+//console.log(getProperty(person, "name"));
